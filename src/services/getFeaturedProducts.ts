dce1d3f42e624e48859cb0b124481a450c2ee159
Anderson Nascimento <anderson.nascimento@meta.com.br>
2023-10-27 09:36:15 -0300

    feat: add revalidation cache next

--- src/services/getFeaturedProducts.ts
+++ src/services/getFeaturedProducts.ts
@@ -3,7 +3,16 @@ import { api } from '@/data/api'
 import { Product } from '@/data/types/product'
 
 export async function getFeaturedProducts(): Promise<Product[]> {
-  const response = await api('/products/featured')
+  const revalidationTime = 60 * 60 // 1 hours
+
+  const response = await api('/products/featured', {
+    cache: 'force-cache',
+    // cache: 'no-cache',
+    next: {
+      // tags: ['products-featured'],
+      revalidate: revalidationTime,
+    },
+  })
 
   const products = await response.json()
 
