662b98a4a2a94c72bad9fea9cfbfa593d2dcc8b3
Anderson Nascimento <anderson.nascimento@meta.com.br>
2023-10-27 09:36:38 -0300

    chore: add delay

--- src/app/api/products/featured/route.ts
+++ src/app/api/products/featured/route.ts
@@ -1,6 +1,8 @@
 import data from '../data.json'
 
 export async function GET() {
+  await new Promise((resolve) => setTimeout(resolve, 5000)) // delay 5s
+
   const featuredProducts = data.products.filter((product) => product.featured)
 
   return new Response(JSON.stringify(featuredProducts), {
