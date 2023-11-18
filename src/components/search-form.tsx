b79cf60795ab88b90a8feb501aab8a8558ec8edb
Anderson Nascimento <anderson.nascimento@meta.com.br>
2023-11-18 12:47:19 -0300

    test: add test for add product to cart

--- src/components/search-form.tsx
+++ src/components/search-form.tsx
@@ -24,16 +24,17 @@ export function SearchForm() {
   }
 
   return (
-    <form onSubmit={handleSearch}>
-      <div className="flex w-[320px] items-center gap-3 rounded-full bg-zinc-900 px-5 py-3 ring-zinc-700">
-        <Search className="w-5 h-5 text-zinc-500" />
-        <input
-          name="q"
-          placeholder="Buscar produtos..."
-          className="flex-1 bg-transparent text-sm outline-none placeholder:text-zinc-500"
-          defaultValue={queryParams ?? ''}
-        />
-      </div>
+    <form
+      onSubmit={handleSearch}
+      className="flex w-[320px] items-center gap-3 rounded-full bg-zinc-900 px-5 py-3 ring-zinc-700"
+    >
+      <Search className="w-5 h-5 text-zinc-500" />
+      <input
+        name="q"
+        placeholder="Buscar produtos..."
+        className="flex-1 bg-transparent text-sm outline-none placeholder:text-zinc-500"
+        defaultValue={queryParams ?? ''}
+      />
     </form>
   )
 }
